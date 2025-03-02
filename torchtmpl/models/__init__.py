424c85efc86376536980d4be103d44b92f7bb5bf
Cziffras <117652887+EmmanuelB-CS@users.noreply.github.com>
2025-03-02 15:37:07 +0100

    importing segformer_custom unet_custom

--- torchtmpl/models/__init__.py
+++ torchtmpl/models/__init__.py
@@ -12,6 +12,8 @@ a model based on a configuration dictionary, input size, and number of classes.
 from .cnn_models import vanilla_cnn
 from .efficientnet_b3_segmentation import EfficientNetB3Segmentation
 from .unet import UNet
+from .torch_unet import UNet_custom
+from .torch_segformer import SegFormer_custom
 
 # Standard Models from smp
 from .smp_models import *
